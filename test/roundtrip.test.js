3ead9e4447edffb47836d2055791dede97728696
jakepruitt <jake@mapbox.com>
2016-06-17 10:09:20 -0700

    Begin vector tile serialization work

---
+++ test/roundtrip.test.js
@@ -0,0 +1,8 @@
+// Read from buffer
+// Write to buffer
+// - Create empty VectorTileFeature
+// - Read geometry buffer from original feature
+// - Add properties and ID's to new feature
+// - Add feature to empty layer
+// - Add layers to tile
+// - Call something like tile.serialize(pbf)
