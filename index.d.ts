03bb951a65fe80d366759576d27d5ab220adbb3d
Harel M <harel.mazor@gmail.com>
2021-10-18 23:17:45 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,14 +2,25 @@ import type Pbf from 'pbf';
 
 export class VectorTile {
     constructor(pbf: Pbf);
+    layers: {[_: string]: VectorTileLayer};
 }
+
 export class VectorTileFeature {
     static types: ['Unknown', 'Point', 'LineString', 'Polygon'];
-    loadGeometry(): {x: number, y: number}[][];
+    extent: number;
+    type: 1 | 2 | 3;
+    id: number;
+    properties: {[_: string]: string | number | boolean};
+    loadGeometry(): Array<Array<Point>>;
     toGeoJSON(x: number, y: number, z: number): GeoJSON.Feature;
     bbox(): [number, number, number, number];
 }
+
 export class VectorTileLayer {
     constructor(pbf: Pbf);
+    version?: number;
+    name: string;
+    extent: number;
+    length: number;
     feature(featureIndex: number): VectorTileFeature;
 }
